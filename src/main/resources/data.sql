4fc854c2dcb616d575d397482dea86f6294bfb7e
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2025-11-19 13:23:18 +0300

    Test 29

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,10 +18,10 @@ INSERT INTO profiles (user_id, bio, avatar_url, created_at, updated_at) VALUES
 (3, 'Experienced Java developer and instructor', '/avatars/alice.jpg', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO courses (title, description, duration, category_id, teacher_id, start_date, created_at, updated_at) VALUES
-('Title 1', 'Description 1', 7,  1, 1, NOW() + INTERVAL '7 days', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Title 2', 'Description 2', 14, 2, 2, NOW() + INTERVAL '14 days', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Title 3', 'Description 3', 30, 3, 1, NOW() + INTERVAL '30 days', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Title 4', 'Description 4', 30, 3, 1, NOW() + INTERVAL '30 days', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('Title 1', 'Description 1', 7,  1, 1, CURRENT_DATE + 7, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Title 2', 'Description 2', 14, 2, 2, CURRENT_DATE + 14, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Title 3', 'Description 3', 30, 3, 1, CURRENT_DATE + 30, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Title 4', 'Description 4', 30, 3, 1, CURRENT_DATE + 30, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO course_tag (course_id, tag_id) VALUES
 (1, 1),
@@ -35,5 +35,5 @@ INSERT INTO lessons (title, module_id) VALUES
 ('Lesson 1', 1);
 
 INSERT INTO assignments (title, due_date, lesson_id) VALUES
-('Assignment 1', NOW() - INTERVAL '1 days', 1),
-('Assignment 2', NOW() + INTERVAL '1 days', 1);
+('Assignment 1', CURRENT_DATE - 1, 1),
+('Assignment 2', CURRENT_DATE + 1, 1);
